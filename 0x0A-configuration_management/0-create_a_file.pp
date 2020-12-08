69a22bbffe372ef9b40bebb2bf6b0347125a27a5
tomasgvgt <tomasgvg.t@gmail.com>
2020-12-08 01:48:17 +0000

    0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-  # Create a file
+#create a file using puppet
 
-  file { 'holberton':
-    path    => '/tmp/holberton',
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet'
+file { 'Holberton':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
   }

92dd5f10952168fd157f818792628b3d7a93ed60
tomasgvgt <60365542+tomasgvgt@users.noreply.github.com>
2021-03-22 13:47:40 -0500

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-#create a file using puppet
-
-file { 'Holberton':
-  path    => '/tmp/holberton',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
-  }

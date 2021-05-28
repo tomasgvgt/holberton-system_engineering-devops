c28b155a4e0e0eae7230b23b0595ebdf4d8ed7d1
tomasgvgt <tomasgvg.t@gmail.com>
2021-05-27 21:46:09 -0500

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 #change limit on nginx
 exec { 'increase-limit':
-  command => "sed -i 's/15/10000/' /etc/default/nginx;
+  command => "sed -i '$ d' /etc/default/nginx;
               service nginx restart",
   path    => '/bin/',
 }
